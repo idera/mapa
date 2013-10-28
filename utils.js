d41d3becb9a8dbf44810051ab696415ddeab584d
root <root@testing.(none)>
2013-10-28 13:25:23 -0300

    corregí la referencia a la capa que usamos para el mapa de referencia

--- utils.js
+++ utils.js
@@ -13,9 +13,9 @@ addOverview = function() {
         layers: [
             new OpenLayers.Layer.WMS(
                 "overview", 
-                "http://wms.ign.gob.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities", 
+                "http://wms.ign.gob.ar/geoserver/wms?", 
                 {
-                    layer: 'capabaseargenmap'
+                    layers: 'capabaseargenmap'
                 }, {
                     buffer: 0, 
                     ratio: 1, 
