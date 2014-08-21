03c97ad7aa4b93233090020b44d0acb1c5233e4e
Cristian Zamar <cristianzamar@gmail.com>
2014-08-21 17:58:38 -0300

    arreglé urls

--- sources.js
+++ sources.js
@@ -15,27 +15,27 @@ var sources = {
         "ptype": "gxp_wmscsource"
     },
     "chaco": {
-        "url": "http: //etisig.chaco.gov.ar/mapasetisig.map",
+        "url": "http://etisig.chaco.gov.ar/mapasetisig.map",
         "title": "IDEChaco",
         "ptype": "gxp_wmssource"
     },
     "formosa": {
-        "url": "http: //idef.formosa.gob.ar/servicios/ows?service=wms&version=1.1.1&request=GetCapabilities",
+        "url": "http://idef.formosa.gob.ar/servicios/ows?service=wms&version=1.1.1&request=GetCapabilities",
         "title": "IDEFormosa",
         "ptype": "gxp_wmscsource"
     },
     "mendoza": {
-        "url": "http: //www.siat.mendoza.gov.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
+        "url": "http://www.siat.mendoza.gov.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
         "title": "SIATMendoza",
         "ptype": "gxp_wmscsource"
     },
     "rionegro": {
-        "url": "http: //ide.extranet.rionegro.gov.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
+        "url": "http://ide.extranet.rionegro.gov.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
         "title": "IDERioNegro",
         "ptype": "gxp_wmscsource"
     },
     "santacruz": {
-        "url": "http: //spm.sitsantacruz.gob.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
+        "url": "http://spm.sitsantacruz.gob.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
         "title": "SITSantaCruz",
         "ptype": "gxp_wmscsource"
     },
@@ -55,32 +55,32 @@ var sources = {
         "ptype": "gxp_wmscsource"
     },
     "inta": {
-        "url": "http: //geointa.inta.gov.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
+        "url": "http://geointa.inta.gov.ar/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities",
         "title": "INTA",
         "ptype": "gxp_wmscsource"
     },
     "indec": {
-        "url": "http: //200.51.91.231/cgi-bin/mapserv?program=/cgibin/mapserv&map=/prosiga/INDEC_WMS_Poblacion.map&SERVICE=WMS&Version=1.1.1&REQUEST=GetCapabilities",
+        "url": "http://200.51.91.231/cgi-bin/mapserv?program=/cgibin/mapserv&map=/prosiga/INDEC_WMS_Poblacion.map&SERVICE=WMS&Version=1.1.1&REQUEST=GetCapabilities",
         "title": "INDEC",
         "ptype": "gxp_wmscsource"
     },
     "energia": {
-        "url": "http: //sig.se.gob.ar/cgi-bin/mapserv6?map=/var/www/html/visor/geofiles/map/mapase.map&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetCapabilities",
+        "url": "http://sig.se.gob.ar/cgi-bin/mapserv6?map=/var/www/html/visor/geofiles/map/mapase.map&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetCapabilities",
         "title": "SecretaríadeEnergía",
         "ptype": "gxp_wmscsource"
     },
     "arba": {
-        "url": "http: //cartoservices.arba.gov.ar/geoserver/wms?service=WMS&request=getcapabilities",
+        "url": "http://cartoservices.arba.gov.ar/geoserver/wms?service=WMS&request=getcapabilities",
         "title": "BuenosAires-ARBA",
         "ptype": "gxp_wmscsource"
     },
     "mapaescolar": {
-        "url": "http: //190.210.101.129/cgi-bin/mapaescolar?service=WMS&request=getcapabilities",
+        "url": "http://190.210.101.129/cgi-bin/mapaescolar?service=WMS&request=getcapabilities",
         "title": "BuenosAires-MapaEscolar",
         "ptype": "gxp_wmscsource"
     },
     "rides": {
-        "url": "http: //rides.producciontucuman.gov.ar/ArcGIS/services/Informacion_Productiva/mapserver/WMSServer?request=getcapabilities",
+        "url": "http://rides.producciontucuman.gov.ar/ArcGIS/services/Informacion_Productiva/mapserver/WMSServer?request=getcapabilities",
         "title": "Tucumán-MDP",
         "ptype": "gxp_wmscsource"
     },
@@ -89,17 +89,17 @@ var sources = {
     },
     "ideracsw": {
         "ptype": "gxp_cataloguesource",
-        "url": "http: //idera.gob.ar/catalogo/srv/en/csw",
+        "url": "http://idera.gob.ar/catalogo/srv/en/csw",
         "title": "CatalogoIDERA"
     },
     "geonode": {
         "ptype": "gxp_cataloguesource",
-        "url": "http: //gxp.opengeo.org/geonetwork/srv/en/csw",
+        "url": "http://gxp.opengeo.org/geonetwork/srv/en/csw",
         "title": "Geonode"
     },
     "pycsw": {
         "ptype": "gxp_cataloguesource",
-        "url": "http: //gxp.opengeo.org/pycsw",
+        "url": "http://gxp.opengeo.org/pycsw",
         "title": "pycsw"
     }
 }
