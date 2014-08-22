f8d99d97b8fcb8c406f0efe6aa3793168d1c6d46
Cristian Zamar <cristianzamar@gmail.com>
2014-08-22 14:16:22 -0300

    agregue archivo de servicios 'sources.php'

--- sources.php
+++ sources.php
@@ -6,21 +6,51 @@ $sources = file_get_contents('sources.js');
 
 if ($_GET['format']=='plain') {
 
-	$sources_json=str_replace("var sources = ","",$sources); 
-	$plano = json_decode($sources_json,true);
-foreach ($plano as $nombre=>$datos){
-	echo $nombre.",".$datos['url'].",";
-} 
+	$jSources=str_replace("var sources = ","",$sources); 
+	$plano = json_decode($jSources,true);
+
+	foreach ($plano as $nombre=>$datos){
+		if (($datos['ptype']=='gxp_wmssource') || ($datos['ptype']=='gxp_wmscsource'))
+			echo $nombre.",".$datos['url'].",";
+	} 
+	
 //	echo json_last_error().json_last_error_msg();
 //	echo "<hr>";
 //	var_dump($plano);
 //	echo "<hr>";
-//	echo "Source json: ".$sources_json;
+//	echo "Source json: ".$jSources;
 
 //	echo "Source original: ".$sources;
 
 }
 // Responder peticion JSON
+else if ($_GET['format']=='xml') {
+	//paso a json para usar array
+	$jSources=str_replace("var sources = ","",$sources); 
+	$aSources = json_decode($jSources,true);
+	
+	//recorro array
+	foreach ($aSources as $nombre=>$datos){
+		//genero nombre del archivo
+		$filename = 'ogc/'.$nombre.'.xml';
+
+		//modifico solo si es wms
+		if (($datos['ptype']=='gxp_wmssource') || ($datos['ptype']=='gxp_wmscsource')) {
+			if (file_exists($filename)) {
+				//reemplazo url con path al xml
+				$aSources[$nombre]['url'] = $filename;
+			} else {
+				//quito source
+				unset($aSources[$nombre]);
+			}
+		}
+	} 
+
+	//vuelta al formato original
+	$jSources = json_encode($aSources);
+	echo "var sources = ".stripslashes($jSources);
+
+}
 else {
 	echo $sources;
 }
